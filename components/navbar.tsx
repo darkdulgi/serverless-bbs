2d0f540a0dfdc9ffd1324c0f9503865625734588
darkdulgi <daniel99285@gmail.com>
2023-06-30 19:27:52 +0900

    카카오 로그인 연동

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function NavBar(){
+export default function NavBar() {
+  const { data: session } = useSession();
+
   return (
     <nav className="bg-gradient-to-r from-indigo-600 to-emerald-600 shadow-md shadow-indigo-200 h-20 text-white flex justify-between items-center font-bold">
       <div className="px-5 text-2xl">
@@ -8,9 +11,21 @@ export default function NavBar(){
           서버리스 게시판
         </Link>
       </div>
+
       <div className="px-5 flex gap-5">
-        <span>로그인</span>
-        <span>설정</span>
+
+        <div className={`flex gap-2 items-center ${session ? "" : "hidden"}`}>
+          <img src={session?.user?.image as string} className="h-8 w-8 rounded-full border-2 border-sky-300" />
+          <span>{session?.user?.name}</span>
+        </div>
+
+        <button
+          className="flex gap-1 items-center"
+          onClick={() => session ? signOut() : signIn("kakao") }>
+          <img src={session ? "logout.svg" : "login.svg"} className="h-8" />
+          <span>{session ? "로그아웃" : "로그인"}</span>
+        </button>
+
       </div>
     </nav>
   );
